544a614290061b0ce2c462482630dd67fb3596b8
Разработчик WebKing <webking.dev@gmail.com>
2015-12-24 12:26:19 +0200

    ошибка в блоке фильтров

--- ~core/block/sb_search_filters.php
+++ ~core/block/sb_search_filters.php
@@ -1,13 +1,15 @@
 <?php
-foreach($dbtree->GetTagsLevelsList($id_category) as $l){
-	$level[$l['tag_level']] = $l; 
-	$level[$l['tag_level']]['tags'] = $dbtree->GetTagsList($id_category, $l['tag_level']);
+if(isset($GLOBALS['CURRENT_ID_CATEGORY'])){
+	foreach($dbtree->GetTagsLevelsList($GLOBALS['CURRENT_ID_CATEGORY']) as $l){
+		$level[$l['tag_level']] = $l; 
+		$level[$l['tag_level']]['tags'] = $dbtree->GetTagsList($GLOBALS['CURRENT_ID_CATEGORY'], $l['tag_level']);
+	}
+	if(isset($level)){
+		$tpl->Assign('level', $level);
+	}
+	$parsed_res = array(
+		'issuccess'	=> true,
+		'html'		=> $tpl->Parse($GLOBALS['PATH_tpl'].'sb_search_filters.tpl')
+	);
 }
-if(isset($level)){
-	$tpl->Assign('level', $level);
-}
-$parsed_res = array(
-	'issuccess'	=> true,
-	'html'		=> $tpl->Parse($GLOBALS['PATH_tpl'].'sb_search_filters.tpl')
-);
 ?>
