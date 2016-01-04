9afe7304cf1f684ba73d2d0b84f453d6018afb06
Alexander <arikito.91@gmail.com>
2016-01-04 16:40:50 +0200

    new ajax structure

---
+++ ~core/contr/ajax.php
@@ -0,0 +1,10 @@
+<?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+	header('Content-Type: text/javascript; charset=utf-8');
+	if(isset($_POST['target']) & file_exists($GLOBALS['PATH_contr'].'ajax/'.$_POST['target'].'.php')){
+		require 'ajax/'.$_POST['target'].'.php';
+	}else{
+		echo 'File doesn\'t exists.';
+	}
+}else{
+	header('Location: '._base_url.'/404');
+}?>
