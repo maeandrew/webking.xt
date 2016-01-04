7a3dcea9970fbbc68f6c0e61caec0f2b5441b6f7
Alexander <arikito.91@gmail.com>
2016-01-04 17:03:41 +0200

    ajax

--- ~core/contr/ajax.php
+++ ~core/contr/ajax.php
@@ -1,6 +1,6 @@
 <?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 	header('Content-Type: text/javascript; charset=utf-8');
-	if(isset($_POST['target']) & file_exists($GLOBALS['PATH_contr'].'ajax/'.$_POST['target'].'.php')){
+	if(isset($_POST['target']) && file_exists($GLOBALS['PATH_contr'].'ajax/'.$_POST['target'].'.php')){
 		require 'ajax/'.$_POST['target'].'.php';
 	}else{
 		echo 'File doesn\'t exists.';
