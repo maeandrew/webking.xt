fed8436a1e34f41c3bdfde49b0a5ca908bc356b5
Alexander <arikito.91@gmail.com>
2016-03-03 10:02:43 +0200

    Авторизация

--- themes/default/js/main.js
+++ themes/default/js/main.js
@@ -834,24 +834,14 @@ $(function(){
 			passwd = form.find('input#passwd').val();
 		form.find('.error').fadeOut();
 		e.preventDefault();
-		$.ajax({
-			url: URL_base+'ajax',
-			type: "POST",
-			dataType : "json",
-			data: {
-				"target": 'auth',
-				"action": 'login',
-				"email": email,
-				"passwd": passwd
-			}
-		}).done(function(data){
-			var parrent = $('.header_nav [for="demo-menu-lower-right"]');
-			parrent.find('.mdl-menu__item .user_name').text(data.member.name);
-			parrent.find('.mdl-menu__item .user_email').text(data.member.email);
-			parrent.find('.mdl-menu__item .user_contr').text(data.member.contragent.name_c);
-			parrent.find('.mdl-menu__item .user_contr_phones').text(data.member.contragent.phones);
-			parrent.find('.mdl-menu__item .user_promo').text(data.member.promo_code);
+		ajax('auth', 'sign_in', {email: email, passwd: passwd}).done(function(data){
+			var parent = $('.header_nav [for="demo-menu-lower-right"]');
 			if(data.errm != 1){
+				parent.find('.mdl-menu__item .user_name').text(data.member.name);
+				parent.find('.mdl-menu__item .user_email').text(data.member.email);
+				parent.find('.mdl-menu__item .user_contr').text(data.member.contragent.name_c);
+				parent.find('.mdl-menu__item .user_contr_phones').text(data.member.contragent.phones);
+				parent.find('.mdl-menu__item .user_promo').text(data.member.promo_code);
 				closeObject('auth');
 				$('.cabinet_btn').removeClass('hidden');
 				$('.login_btn').addClass('hidden');
