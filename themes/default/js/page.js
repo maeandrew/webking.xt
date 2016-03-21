0de46adf083320fa2e8ee414dac27daac8074f73
NadiiaKov <webking.dev3@gmail.com>
2016-03-21 19:04:03 +0200

    Работа над группой из 10 страниц
    Изменен скрипт
    (не окончательный вариант)

--- themes/default/js/page.js
+++ themes/default/js/page.js
@@ -1,51 +1,72 @@
 $(document).ready(function () {
 	var posBasic = 0;
 	// Для кнопки "btn_plus"
-	if ($(document).width() < 900) {
-			$('.blockOfText a').removeClass('.pos-right, .pos-left');
-		};
-	$('.btn_plus').click(function(e) {
-		e.preventDefault();
-		var target = $(this).closest('.blockline').next(),
-			  contentCenter = posBasic;
+	if ($(document).width() > 900) {
+		$('.btn_plus').click(function(e) {
+			e.preventDefault();
+			var target = $(this).closest('.blockline').next(),
+				  contentCenter = posBasic;
 
-		
-		if($(this).hasClass('active')){
-			target.slideUp(); //скрывает блок
-			$(this).removeClass('active');
-		}else{
-			if($(this).hasClass('pos-right')){
-				posBasic = $(this).css('right');
-			}else if($(this).hasClass('pos-right--beta')) {
-		posBasic = $(this).css('right');
-	  }else{
-				posBasic = $(this).css('left');
-			}
-			target.slideDown();
-			$(this).addClass('active');
-	  $('html, body').stop().animate({
-				'scrollTop': target.offset().top - 
-			($('header').outerHeight() + $('.blockline').innerHeight() - 130)
-	   // + $(this).find('span').outerHeight())
-			}, 900);      
-			// $('html, body').stop().animate({
-			// 	'scrollTop': target.offset().top - 
-			// ($('header').outerHeight() + $(this).find('span').outerHeight())
-			// }, 900);
-		
-		contentCenter = $(".blockline").width()/2
-		- $(this).width()/2;
-	}
-	// центрирует btn_plus по середине blockline
-		if($(this).hasClass('pos-right')){
-			$(this).css({'right': contentCenter});
-		}else if($(this).hasClass('pos-right--beta')){
-			$(this).css({'right': contentCenter});
-		}else {
-			$(this).css({'left': contentCenter});
+			
+			if($(this).hasClass('active')){
+				target.slideUp(); //скрывает блок
+				$(this).removeClass('active');
+			}else{
+				if($(this).hasClass('pos-right')){
+					posBasic = $(this).css('right');
+				}else if($(this).hasClass('pos-right--beta')) {
+			posBasic = $(this).css('right');
+		  }else{
+		  	posBasic = $(this).css('left');
+				}
+				target.slideDown();
+				$(this).addClass('active');
+		  		$('html, body').stop().animate({
+					'scrollTop': target.offset().top - 
+				($('header').outerHeight() + $('.blockline').innerHeight() - 130)
+		   // + $(this).find('span').outerHeight())
+				}, 900);      
+				// $('html, body').stop().animate({
+				// 	'scrollTop': target.offset().top - 
+				// ($('header').outerHeight() + $(this).find('span').outerHeight())
+				// }, 900);
+			
+			contentCenter = $(".blockline").width()/2
+			- $(this).width()/2;
 		}
-	});
-
+		// центрирует btn_plus по середине blockline
+			if($(this).hasClass('pos-right')){
+				$(this).css({'right': contentCenter});
+			}else if($(this).hasClass('pos-right--beta')){
+				$(this).css({'right': contentCenter});
+			}else {
+				$(this).css({'left': contentCenter});
+			}
+		});
+	}else{
+		$('.btn_plus').click(function(e) {
+			e.preventDefault();
+			var target = $(this).closest('.blockline').next();				
+			
+			if($(this).hasClass('active')){
+				target.slideUp(); //скрывает блок
+				$(this).removeClass('active');
+			}else{				
+				target.slideDown();
+				$(this).addClass('active');
+		  	
+		  	$('html, body').stop().animate({
+					'scrollTop': target.offset().top - 
+				($('header').outerHeight() + $('.blockline').innerHeight() - 130)
+		   // + $(this).find('span').outerHeight())
+				}, 900);      
+				// $('html, body').stop().animate({
+				// 	'scrollTop': target.offset().top - 
+				// ($('header').outerHeight() + $(this).find('span').outerHeight())
+				// }, 900);
+			}
+		});
+	}
 
 
 	$('.a_estimateLoad').click(function(event) {
