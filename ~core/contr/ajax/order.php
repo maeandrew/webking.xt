a52675d64beb3ded6ffe68b6d7d46d6e22ce7b01
Andrey.k <webking.dev@gmail.com>
2016-04-05 11:42:20 +0300

    статусы заказа

--- ~core/contr/ajax/order.php
+++ ~core/contr/ajax/order.php
@@ -1,12 +1,13 @@
 <?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 	header('Content-Type: text/javascript; charset=utf-8');
+	$order = new Orders;
 	if(isset($_POST['action'])){
 		switch($_POST['action']){
 			case "CancelOrder":
-				echo json_encode(true);
+				echo json_encode($order->UpdateStatus($_POST['id_order'], 5));
 				break;
 			case "DeleteOrder":
-				echo json_encode(true);
+				echo json_encode($order->OffUserOrder($_POST['id_order']));
 				break;
 			default:
 				break;
