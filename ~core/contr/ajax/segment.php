5a47a0591ab056214b83d9494485d729f56e643d
Andrey.k <webking.dev@gmail.com>
2016-04-15 13:16:58 +0300

    segm

--- ~core/contr/ajax/segment.php
+++ ~core/contr/ajax/segment.php
@@ -21,6 +21,14 @@ if ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
                     exit();
                 } else if ($_POST['type'] == 0){
                     $navigation = $dbtree->GetCats(array('id_category', 'category_level', 'name', 'category_banner', 'banner_href', 'translit', 'pid'), 1);
+                    foreach($navigation as &$l1){
+                        $level2 = $dbtree->GetSubCats($l1['id_category'], 'all');
+                        foreach($level2 as &$l2){
+                            $level3 = $dbtree->GetSubCats($l2['id_category'], 'all');
+                            $l2['subcats'] = $level3;
+                        }
+                        $l1['subcats'] = $level2;
+                    }
                     $cat = $nav->generateNavigation($navigation);
                     echo $cat;
                     exit();
