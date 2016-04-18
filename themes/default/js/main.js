b141731cf9291d491f24de676e68392eef694f41
Andrey.k <webking.dev@gmail.com>
2016-04-18 17:44:24 +0300

    segm

--- themes/default/js/main.js
+++ themes/default/js/main.js
@@ -28,9 +28,9 @@ $(document).ready(function() {
 	if(($('.second_nav > li.active > ul[data-lvl="2"] > li.active:has(ul)')).length == 0 || $('.second_nav ul[data-lvl="3"] > li').hasClass('active')) { 
 		$('.main_nav li:last-child').removeClass('hidden');
 	}
-	if(($('.second_nav li.active').find('li.active')).length == 0) {
-		$('.main_nav li:last-child').addClass('hidden');
-	}
+	//if(($('.second_nav li.active').find('li.active')).length == 0) {
+	//	$('.main_nav li:last-child').addClass('hidden');
+	//}
 });
 
 $(function(){
