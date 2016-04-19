1d0d289ee81b82f13a3fe010cce52c67dd7a42f1
Andrey.k <webking.dev@gmail.com>
2016-04-19 15:49:43 +0300

    фильтрация, сегментация

--- ~core/sys/link_c.php
+++ ~core/sys/link_c.php
@@ -31,13 +31,12 @@ class Link {
 			if(isset($GLOBALS['Price_range']) && $GLOBALS['Price_range'] !== ''){
 				$price_range = 'price_range='.$GLOBALS['Price_range'][0].','.$GLOBALS['Price_range'][1];
 			}
-			if(isset($GLOBALS['Segment']) && $GLOBALS['Segment'] !== ''){
-				$str_segment = 'segment='.$GLOBALS['Segment'];
-			}
 		}else{
 			$clear = $params['clear'];
 		}
-		if(isset($params)&&$params !==''){
+		if(isset($GLOBALS['Segment']) && $GLOBALS['Segment'] !== ''){
+			$str_segment = 'segment='.$GLOBALS['Segment'];
+		}
 		foreach($params as $key => $param){
 			switch($key){
 				case 'filters':
@@ -79,7 +78,7 @@ class Link {
 					break;
 			}
 		}
-		}
+
 
 		// if(isset($GLOBALS['Rewrite']) && $GLOBALS['Rewrite'] == $rewrite) {
 			if(isset($filter) && is_array($filter) && !empty($filter)){
