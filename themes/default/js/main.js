c2dc7c3c4e73954bb7e92b60191709aa10a54257
NadiiaKov <webking.dev3@gmail.com>
2016-06-17 17:23:19 +0300

    Функционал удаления товара из списка "избранных" и "листа ожидания"
    Переделано

--- themes/default/js/main.js
+++ themes/default/js/main.js
@@ -115,66 +115,70 @@ $(function(){
 	// 	AddFavorite(event,id_product);
 	// });
 
-	//Удаление Избранного товара из списка
+	//Удаление избранного товара из списка
 	$('body').on('click', '.remove_favor_js', function(e){
 		e.preventDefault();
-		var id_product = $(this).closest('.favorite_js').attr('data-idproduct'),
+		var id_product = $(this).closest('.favorite_js').data('idproduct'),
 			clicked = $(this);
-		clicked.addClass('clicked_js');
-		setTimeout(function() {
-			addLoadAnimation(clicked.closest('.favorite_js'));
-			if (clicked.hasClass('confirmDel_js')) {
-				ajax('product', 'del_favorite', {id_product: id_product}).done(function(data){
-					if (data.fav_count > 0) {
-						clicked.closest('.favorite_js').remove();
-						$('.userChoiceFav').text('('+data.fav_count+')');
-						var data = {message: 'Товар удален из списка избранных товаров'};
-						var snackbarContainer = document.querySelector('#snackbar');
-						snackbarContainer.MaterialSnackbar.showSnackbar(data);
-					}else{
-						$('#favorites').html('<h5>У Вас нет избранных товаров</h5>');
-						$('.userChoiceFav').text('(0)');
-					}
-					clicked.removeClass('clicked_js');
-				});
-			}
-		}, 2000);
+		$('#confirmDelItem').data('delIdItem', id_product).data('list', "favor");
+		clicked.addClass('clicked_js').closest('.tableRow').addClass('flag_js');
 	});
 
 	//Удаление товара из листа ожидания
 	$('body').on('click', '.remove_waitinglist_js', function(e){
 		e.preventDefault();
-		var id_product = $(this).closest('.waiting_list_js').attr('data-idproduct'),
+		var id_product = $(this).closest('.waiting_list_js').data('idproduct'),
 			clicked = $(this);
-		clicked.addClass('clicked_js');
-		setTimeout(function() {
-			addLoadAnimation(clicked.closest('.waiting_list_js'));
-			if (clicked.hasClass('confirmDel_js')) {
-				ajax('product', 'del_from_waitinglist', {id_product: id_product}).done(function(data){
-					console.log(data);
-					if (data.fav_count > 0) {
-						clicked.closest('.waiting_list_js').remove();
-						$('.userChoiceWait').text('('+data.fav_count+')');
-						var data = {message: 'Товар удален из листа ожидания'};
-						var snackbarContainer = document.querySelector('#snackbar');
-						snackbarContainer.MaterialSnackbar.showSnackbar(data);
-					}else{
-						$('#waiting_list').html('<h5>Лист ожидания пуст</h5>');
-						$('.userChoiceWait').text('(0)');
-					}
-				}).fail(function(data){
-					console.log('Error!');
-				});
-			}
-		}, 2000);
+		$('#confirmDelItem').data('delIdItem', id_product).data('list', "waiting");
+		clicked.addClass('clicked_js').closest('.tableRow').addClass('flag_js');
 	});
 
+	//Подтверждение удаления из листа ожидания/списка избранных
 	$('#confirmDelItem').on('click', '.deleteBtn_js', function(){
-		$('.clicked_js').addClass('confirmDel_js');
+		var id_product = $('#confirmDelItem').data('delIdItem'),
+			clicked = $('.clicked_js');
 		closeObject('confirmDelItem');
+		if ($('#confirmDelItem').data('list') == 'favor') {
+			addLoadAnimation('.flag_js');
+			ajax('product', 'del_favorite', {id_product: id_product}).done(function(data){
+				if (data.fav_count > 0) {
+					clicked.closest('.favorite_js').remove();
+					$('.userChoiceFav').text('('+data.fav_count+')');
+					var data = {message: 'Товар удален из списка избранных товаров'};
+					var snackbarContainer = document.querySelector('#snackbar');
+					snackbarContainer.MaterialSnackbar.showSnackbar(data);
+				}else{
+					$('#favorites').html('<h5>У Вас нет избранных товаров</h5>');
+					$('.userChoiceFav').text('(0)');
+				}				
+			}).fail(function(data){
+				console.log('Error!');
+			});
+		}else if ($('#confirmDelItem').data('list') == 'waiting') {
+			addLoadAnimation('.flag_js');
+			ajax('product', 'del_from_waitinglist', {id_product: id_product}).done(function(data){
+				if (data.fav_count > 0) {
+					clicked.closest('.waiting_list_js').remove();
+					$('.userChoiceWait').text('('+data.fav_count+')');
+					var data = {message: 'Товар удален из листа ожидания'};
+					var snackbarContainer = document.querySelector('#snackbar');
+					snackbarContainer.MaterialSnackbar.showSnackbar(data);
+				}else{
+					$('#waiting_list').html('<h5>Лист ожидания пуст</h5>');
+					$('.userChoiceWait').text('(0)');
+				}
+			}).fail(function(data){
+				console.log('Error!');
+			});
+		}
+		clicked.removeClass('clicked_js');
+		$('.flag_js').removeClass('flag_js');
 	});
+	
 	$('#confirmDelItem').on('click', '.cancelBtn_js', function(){
-		$('.clicked_js').removeClass('confirmDel_js');
+		$('#confirmDelItem').removeData('delIdItem');
+		$('.clicked_js').removeClass('clicked_js');
+		$('.flag_js').removeClass('flag_js');
 		closeObject('confirmDelItem');
 	});
 
