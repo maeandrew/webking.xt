44566b51ebb5699b903c2dfeebdccf8bdedc9025
Andrey.k <webking.dev@gmail.com>
2016-06-29 15:22:50 +0300

    дублирование товара

--- ~core/model/products_c.php
+++ ~core/model/products_c.php
@@ -1785,14 +1785,14 @@ class Products {
 			$f['price_opt_otpusk_usd'] = round($arr['price_opt_otpusk'] / $supp_fields['currency_rate'], 2);
 			$f['price_mopt_otpusk_usd'] = round($arr['price_mopt_otpusk'] / $supp_fields['currency_rate'], 2);
 		}else{
-			$f['price_opt_otpusk'] = round($arr['price_opt_otpusk'] * $supp_fields['currency_rate'], 2);
-			$f['price_mopt_otpusk'] = round($arr['price_mopt_otpusk'] * $supp_fields['currency_rate'], 2);
-			$f['price_opt_otpusk_usd'] = trim($arr['price_opt_otpusk']);
-			$f['price_mopt_otpusk_usd'] = trim($arr['price_mopt_otpusk']);
+			$f['price_opt_otpusk'] = round($arr['price_opt_otpusk_usd'] * $supp_fields['currency_rate'], 2);
+			$f['price_mopt_otpusk'] = round($arr['price_mopt_otpusk_usd'] * $supp_fields['currency_rate'], 2);
+			$f['price_opt_otpusk_usd'] = trim($arr['price_opt_otpusk_usd']);
+			$f['price_mopt_otpusk_usd'] = trim($arr['price_mopt_otpusk_usd']);
 		}
 		$f['price_opt_recommend'] = $f['price_opt_otpusk'] * $supp_fields['koef_nazen_opt'];
 		$f['price_mopt_recommend'] = $f['price_mopt_otpusk'] * $supp_fields['koef_nazen_mopt'];
-
+		$f['edited'] = date('Y-m-d');
 		$this->db->StartTrans();
 		//Заполнение массива для проверки на совпадения
 		$check['id_product'] = $arr['id_product'];
