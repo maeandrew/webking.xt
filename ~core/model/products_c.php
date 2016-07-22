8aa71566e39c86ce1a03dd34c7bd6b1a10e6da82
Alexander <arikito.91@gmail.com>
2016-07-22 16:06:02 +0300

    Экспорт импорт товаров в админке

--- ~core/model/products_c.php
+++ ~core/model/products_c.php
@@ -2585,7 +2585,7 @@ class Products {
 	 * @param [type] $id_product [description]
 	 */
 	public function GetCatsOfProduct($id_product){
-		$sql = "SELECT cp.id_category, cp.main
+		$sql = "SELECT cp.id_category, cp.main, c.art
 			FROM "._DB_PREFIX_."cat_prod AS cp
 			LEFT JOIN "._DB_PREFIX_."category AS c
 				ON c.id_category = cp.id_category
