4da8427299695f48de8a4e549cd24252d1bff4de
Webkingdev <webking.dev2@gmail.com>
2016-07-25 14:42:53 +0300

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		cssmin: {
+			options: {
+				keepBreaks: true,
+				advanced: false
+			},
+			target: {
+				files: [{					
+					expand: true,
+					cwd: 'themes/default/css',
+					src: ['*.css', '!*.min.css'],
+					ext: '.min.css'
+				}]
+			}
+		}
+	});
+
+	// Load the plugin that provides the task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify', 'cssmin']);
+};
