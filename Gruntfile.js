eded3170d28c4e96c7833a6e4cb62d63a4277cb5
Alexander <arikito.91@gmail.com>
2016-07-25 14:52:02 +0300

    ignore node_modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
 	});
 
 	// Load the plugin that provides the task.
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	// Default task(s).
