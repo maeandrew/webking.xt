26810bc80eec7ec3c029c8570784dd4d378b4c8c
Alexander <arikito.91@gmail.com>
2016-08-01 09:42:56 +0300

    noprice products sort

--- ~core/model/products_c.php
+++ ~core/model/products_c.php
@@ -4841,7 +4841,8 @@ class Products {
 
 	public  function GetNopriceProducts($limit = false){
 		$sql = "SELECT id_product, `name`, translit, price_mopt, price_opt FROM "._DB_PREFIX_."product
-				WHERE (price_mopt = 0 AND price_opt <> 0) OR (price_opt = 0 AND price_mopt <> 0) AND visible = 1".($limit !== false?$limit:'');
+				WHERE (price_mopt = 0 AND price_opt <> 0) OR (price_opt = 0 AND price_mopt <> 0) AND visible = 1".($limit !== false?$limit:'')."
+				ORDER BY price_opt";
 		if(!$res = $this->db->GetArray($sql)){
 			return false;
 		}
