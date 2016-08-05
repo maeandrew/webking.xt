42c700d56be9d99df00242ef6bb276dfb1ae31d6
Andrey.k <webking.dev@gmail.com>
2016-08-05 17:55:19 +0300

    1

--- ~core/contr/ajax/cart.php
+++ ~core/contr/ajax/cart.php
@@ -372,17 +372,17 @@ if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 						$customer_data['last_order'] = $order->GetLastOrder($id_user);
 						//$res['status'] = true;
 						$res = '<div class="customer_main_info">
-									<input hidden value="'.$id_user.'">
-									<p>ФИО: '.$customer_data['name']?$customer_data['name']:null.'</p>
-									<p>email: '.$customer_data['email']?$customer_data['email']:null.'</p>
-									<p>Баланс: '.$customer_data['balance']?$customer_data['balance']:null.'</p>
-									<p>Последний заказ: '.$customer_data['last_order']?$customer_data['last_order']:null.'</p>
-									<p>Активность: '.$customer_data['active'] ==1?'Да':'Нет'.'</p>
+									<input type="hidden" value="'.$id_user.'">
+									<p>ФИО: '.($customer_data['name']?$customer_data['name']:null).'</p>
+									<p>email: '.($customer_data['email']?$customer_data['email']:null).'</p>
+									<p>Баланс: '.($customer_data['balance']?$customer_data['balance']:null).'</p>
+									<p>Последний заказ: '.($customer_data['last_order']?$customer_data['last_order']:null).'</p>
+									<p>Активность: '.($customer_data['active'] ==1?'Да':'Нет').'</p>
 								</div>
 								<div class="bonus_block">
-									<p>Бонусная карта: №'.$customer_data['bonus_card']?$customer_data['bonus_card']:null.'</p>
-									<p>Бонусный баланс: '.$customer_data['bonus_balance']?$customer_data['bonus_balance']:null.' грн.</p>
-									<p>Бонусный процент: '.$customer_data['bonus_discount']?$customer_data['bonus_discount']:null.'%</p>
+									<p>Бонусная карта: №'.($customer_data['bonus_card']?$customer_data['bonus_card']:null).'</p>
+									<p>Бонусный баланс: '.($customer_data['bonus_balance']?$customer_data['bonus_balance']:null).' грн.</p>
+									<p>Бонусный процент: '.($customer_data['bonus_discount']?$customer_data['bonus_discount']:null).'%</p>
 								</div>';
 					}
 				}else {
