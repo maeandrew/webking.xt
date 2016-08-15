1391d675f47e554eb832baddc142947cedb97688
Andrey.k <webking.dev@gmail.com>
2016-08-15 08:58:36 +0300

    1

--- ~core/contr/ajax/order.php
+++ ~core/contr/ajax/order.php
@@ -1,21 +1,16 @@
 <?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 	header('Content-Type: text/javascript; charset=utf-8');
-	$order = new Orders;
+	$Orders = new Orders;
 	if(isset($_POST['action'])){
 		switch($_POST['action']){
 			case "CancelOrder":
-				echo json_encode($order->UpdateStatus($_POST['id_order'], 5));
+				echo json_encode($Orders->UpdateStatus($_POST['id_order'], 5));
 				break;
 			case "DeleteOrder":
-				echo json_encode($order->OffUserOrder($_POST['id_order']));
+				echo json_encode($Orders->OffUserOrder($_POST['id_order']));
 				break;
 			case "restoreDeleted":
-				$Orders = new Orders();
-				ob_start();
-				if (isset($_POST['restore'])){
-					$id_order = $_POST['id_order'];
-					$order->RestoreDeleted($id_order);
-				}
+				echo json_encode($Orders->RestoreDeleted($_POST['id_order']));
 				break;
 			default:
 				break;
