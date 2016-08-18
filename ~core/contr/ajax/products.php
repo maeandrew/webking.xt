41d7a2cb3dd3ba58e8e135a0e63ec0e4ec9e4919
Andrey.k <webking.dev@gmail.com>
2016-08-18 17:21:26 +0300

    1

--- ~core/contr/ajax/products.php
+++ ~core/contr/ajax/products.php
@@ -197,31 +197,31 @@ if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 										</div>
 									</div>
 									<div class="actions">';
-									if($item['indexation'] != 0){
-										$prod_arr .='<a href="/adm/productedit/'.$item['id_product'].'" class="icon-font btn-m-blue" target="_blank" title="Редактировать">e</a>';
-									}
-									$prod_arr .='<a href="'._base_url.'/'.$item['translit'].'.html" class="icon-font btn-m-green" target="_blank" title="Посмотреть на сайте">v</a>
-									</div>';
-									if(is_array($item['images'])){
-									$prod_arr .='<div class="prodImages">';
-											foreach($item['images'] as $image){
-												$prod_arr .='<!-- <img src="'.G::GetImageUrl($image['src'], 'thumb').'"'.$image['visible'] == 0?' class="imgopacity"':'' .' alt=""> -->
-												<img src="'._base_url.$image['src'].'"'.$image['visible'] == 0?' class="imgopacity"':''.' alt="">
-												<!-- 	<img src="/images/noavatar.png"'.$image['visible'] == 0?' class="imgopacity"':''.' alt=""> -->';
-											}
-									$prod_arr .='</div>';
-									}
-									if(is_array($item['videos'])){
-										$prod_arr .='<div class="prodVideos">';
-										foreach($item['videos'] as $video){
-										$prod_arr .='<a href="<?=$video?>" target="blank">
-													<img src="/images/video_play.png" alt="play">
-													<span class="name">'.$video.'</span>
-												</a>';
-											}
-										$prod_arr .='</div>';
-									}
-									$prod_arr .='</div>';
+					if($item['indexation'] != 0){
+						$prod_arr .='<a href="/adm/productedit/'.$item['id_product'].'" class="icon-font btn-m-blue" target="_blank" title="Редактировать">e</a>';
+					}
+					$prod_arr .='<a href="'._base_url.'/'.$item['translit'].'.html" class="icon-font btn-m-green" target="_blank" title="Посмотреть на сайте">v</a>
+							</div>';
+					if(is_array($item['images'])){
+					$prod_arr .='<div class="prodImages">';
+						foreach($item['images'] as $image){
+							$prod_arr .='<!-- <img src="'.G::GetImageUrl($image['src'], 'thumb').'"'.$image['visible'] == 0?' class="imgopacity"':'' .' alt=""> -->
+							<img src="'._base_url.$image['src'].'"'.$image['visible'] == 0?' class="imgopacity"':''.' alt="">
+							<!-- 	<img src="/images/noavatar.png"'.$image['visible'] == 0?' class="imgopacity"':''.' alt=""> -->';
+						}
+					$prod_arr .='</div>';
+					}
+					if(is_array($item['videos'])){
+						$prod_arr .='<div class="prodVideos">';
+						foreach($item['videos'] as $video){
+						$prod_arr .='<a href="<?=$video?>" target="blank">
+									<img src="/images/video_play.png" alt="play">
+									<span class="name">'.$video.'</span>
+								</a>';
+							}
+						$prod_arr .='</div>';
+					}
+					$prod_arr .='</div>';
 				}
 				echo $prod_arr;
 				break;
