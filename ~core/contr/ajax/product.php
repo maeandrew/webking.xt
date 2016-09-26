b28baceb61a0dfecb8d70b0b98aa69e9108595d0
Alexander <arikito.91@gmail.com>
2016-09-26 17:32:00 +0300

    price info

--- ~core/contr/ajax/product.php
+++ ~core/contr/ajax/product.php
@@ -296,46 +296,88 @@ if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 				$Products = new Products();
 				$Products->SetFieldsById($_POST['id_product']);
 				$product = $Products->fields;
+				$opt_corrections = explode(';', $GLOBALS['CONFIG']['correction_set_'.$product['opt_correction_set']]);
+				$mopt_corrections = explode(';', $GLOBALS['CONFIG']['correction_set_'.$product['mopt_correction_set']]);
 				$helper = '	<div class="prices_table_title">
-								<p>Таблица цены товара</p>
+								<h4>Система скидок</h4>
 							</div>
 							<table class="prices_table">
 								<tr>
-									<td rowspan="2" class="summ_title_cell">Сумма заказа</td>
-									<td colspan="2">Цена товара</td>
+									<th colspan="3">Покупка от '.$product['inbox_qty'].' '.$product['units'].'</th>
 								</tr>
 								<tr>
-									<td>от '.$product['min_mopt_qty'].' шт.</td>
-									<td>от '.$product['inbox_qty'].' шт.</td>
+									<th class="title_column">Сумма заказа</th>
+									<th>Цена, грн</th>
+									<th>Скидка, %</th>
 								</tr>
 								<tr>
-									<td class="title_row">до '.$GLOBALS['CONFIG']['retail_order_margin'].' грн.</td>
-									<td>'.number_format($product['prices_mopt'][3], 2, ",", "").' грн.</td>
-									<td>'.number_format($product['prices_opt'][3], 2, ",", "").' грн.</td>
+									<td class="title_column">Партнерская (более '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_opt'][0], 2, ",", "").'</td>
+									<td>'.(100-$opt_corrections[0]*100).'</td>
 								</tr>
 								<tr>
-									<td class="title_row">от '.$GLOBALS['CONFIG']['retail_order_margin'].' до '.$GLOBALS['CONFIG']['wholesale_order_margin'].' грн.</td>
-									<td>'.number_format($product['prices_mopt'][2], 2, ",", "").' грн.</td>
-									<td>'.number_format($product['prices_opt'][2], 2, ",", "").' грн.</td>
+									<td class="title_column">Диллерская (от '.$GLOBALS['CONFIG']['wholesale_order_margin'].' до '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_opt'][1], 2, ",", "").'</td>
+									<td>'.(100-$opt_corrections[1]*100).'</td>
 								</tr>
 								<tr>
-									<td class="title_row">от '.$GLOBALS['CONFIG']['wholesale_order_margin'].' до '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.</td>
-									<td>'.number_format($product['prices_mopt'][1], 2, ",", "").' грн.</td>
-									<td>'.number_format($product['prices_opt'][1], 2, ",", "").' грн.</td>
+									<td class="title_column">Оптовая (от '.$GLOBALS['CONFIG']['retail_order_margin'].' до '.$GLOBALS['CONFIG']['wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_opt'][2], 2, ",", "").'</td>
+									<td>'.(100-$opt_corrections[2]*100).'</td>
 								</tr>
 								<tr>
-									<td class="title_row">более '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.</td>
-									<td>'.number_format($product['prices_mopt'][0], 2, ",", "").' грн.</td>
-									<td>'.number_format($product['prices_opt'][0], 2, ",", "").' грн.</td>
+									<td class="title_column">Розничная (до '.$GLOBALS['CONFIG']['retail_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_opt'][3], 2, ",", "").'</td>
+									<td>'.(100-$opt_corrections[3]*100).'</td>
 								</tr>
 							</table>
-							<div class="msg-info bonus_info">
-								<div class="msg_icon">
-									<i class="material-icons"></i>
-								</div>
-								<p class="msg_title">!</p>
-								<p class="msg_text">Тут текст про бонусы</p>
-							</div>';
+							<table class="prices_table">
+								<tr>
+									<th colspan="3">Покупка от '.$product['min_mopt_qty'].' '.$product['units'].'</th>
+								</tr>
+								<tr>
+									<th class="title_column">Сумма заказа</th>
+									<th>Цена, грн</th>
+									<th>Скидка, %</th>
+								</tr>
+								<tr>
+									<td class="title_column">Партнерская (более '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_mopt'][0], 2, ",", "").'</td>
+									<td>'.(100-$mopt_corrections[0]*100).'</td>
+								</tr>
+								<tr>
+									<td class="title_column">Диллерская (от '.$GLOBALS['CONFIG']['wholesale_order_margin'].' до '.$GLOBALS['CONFIG']['full_wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_mopt'][1], 2, ",", "").'</td>
+									<td>'.(100-$mopt_corrections[1]*100).'</td>
+								</tr>
+								<tr>
+									<td class="title_column">Оптовая (от '.$GLOBALS['CONFIG']['retail_order_margin'].' до '.$GLOBALS['CONFIG']['wholesale_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_mopt'][2], 2, ",", "").'</td>
+									<td>'.(100-$mopt_corrections[2]*100).'</td>
+								</tr>
+								<tr>
+									<td class="title_column">Розничная (до '.$GLOBALS['CONFIG']['retail_order_margin'].' грн.)</td>
+									<td>'.number_format($product['prices_mopt'][3], 2, ",", "").'</td>
+									<td>'.(100-$mopt_corrections[3]*100).'</td>
+								</tr>
+							</table>
+							<table class="bonus_table">
+								<tr>
+									<th colspan="3">Бонусная программа</th>
+								</tr>
+								<tr>
+									<td>акривируй бонусную програму получи 20 грн</td>
+								</tr>
+								<tr>
+									<td>1% от суммы заказа - постояно</td>
+								</tr>
+								<tr>
+									<td>2% от суммы заказа - на второй заказ в течении 30 дней</td>
+								</tr>
+								<tr>
+									<td>3% от суммы заказа - на третий и более заказ в течении 30 дней</td>
+								</tr>
+							</table>';
 				echo $helper;
 				break;
 			default:
