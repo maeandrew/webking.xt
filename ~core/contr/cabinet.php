db104c5651392d9a507c8dd208b4ce0a9d0a24b0
Alexander <arikito.91@gmail.com>
2016-11-24 11:09:37 +0200

    чистка кода кабинета

--- ~core/contr/cabinet.php
+++ ~core/contr/cabinet.php
@@ -9,14 +9,12 @@ $Page = new Page();
 $Page->PagesList();
 $tpl->Assign('list_menu', $Page->list);
 unset($parsed_res);
-$User = new Users();
-$User->SetUser($_SESSION['member']);
 // Блок навигации в кабинете
 require($GLOBALS['PATH_block'].'sb_cabinet_navigation.php');
 if(isset($parsed_res) && $parsed_res['issuccess'] == true){
 	$tpl_center .= $parsed_res['html'];
 }
-switch($User->fields['gid']){
+switch($Users->fields['gid']){
 	case _ACL_CUSTOMER_:
 		$header = 'Кабинет клиента';
 		if(isset($_SESSION['member']['promo_code']) && $_SESSION['member']['promo_code'] != ''){
@@ -48,16 +46,10 @@ switch($User->fields['gid']){
 				require($GLOBALS['PATH_block'].'cp_cab_personal_customer.php');
 			}
 		}
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_SUPPLIER_:
 		$header = 'Кабинет поставщика';
 		require($GLOBALS['PATH_block'].'cp_cab_supplier.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_CONTRAGENT_:
 		$header = 'Кабинет контрагента';
@@ -66,16 +58,10 @@ switch($User->fields['gid']){
 		}else{
 			require($GLOBALS['PATH_block'].'cp_cab_contragent.php');
 		}
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_MANAGER_:
 		$header = 'Кабинет менеджера';
 		require($GLOBALS['PATH_block'].'cp_cab_manager.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_DILER_:
 		$header = 'Кабинет дилера';
@@ -84,42 +70,33 @@ switch($User->fields['gid']){
 		}else{
 			require($GLOBALS['PATH_block'].'cp_cab_diler.php');
 		}
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
     case _ACL_M_DILER_:
 		$header = 'Кабинет';
 		require($GLOBALS['PATH_block'].'cp_cab_m_diler.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_ANONYMOUS_:
 		$header = 'История заказов анонимных клиентов';
 		require($GLOBALS['PATH_block'].'cp_cab_anonim.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_TERMINAL_:
 		$header = 'Кабинет терминального клиента';
 		require($GLOBALS['PATH_block'].'cp_cab_terminal.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
 		break;
 	case _ACL_SUPPLIER_MANAGER_:
 		$header = 'Кабинет менеджера поставщиков';
 		require($GLOBALS['PATH_block'].'cp_cab_supplier_manager.php');
-		if(true == $parsed_res['issuccess']){
-			$tpl_center .= $parsed_res['html'];
-		}
+		break;
+	case _ACL_DEPARTMENT_:
+		$header = 'Кабинет пункта выдачи';
+		require($GLOBALS['PATH_block'].'cp_cab_department.php');
 		break;
 	default:
 		header('Location: /adm/');
 		break;
 }
-
+if(true == $parsed_res['issuccess']){
+	$tpl_center .= $parsed_res['html'];
+}
 $tpl->Assign('header', $header);
 G::metaTags(array('page_title' => $header));
