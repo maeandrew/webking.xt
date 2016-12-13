4ffa4108e6cfa2a16e3f34e7db084dbb03fe054d
Alexander <arikito.91@gmail.com>
2016-12-13 12:09:43 +0200

    Агентское соглашение

--- ~core/block/cp_cab_agent_customer.php
+++ ~core/block/cp_cab_agent_customer.php
@@ -13,6 +13,7 @@ if(isset($_REQUEST['confirm_agent']) && !G::isAgent() && $Users->ConfirmAgent())
 }
 
 $orders = $Users->GetAgentInfo($_SESSION['member']['id_user']);
+$total_bonus = 0;
 if(!empty($orders)){
 	foreach($orders as $order){
 		if($order['id_order_status'] == 2){
@@ -21,13 +22,17 @@ if(!empty($orders)){
 			}else{
 				$history[date('d.m.Y', $order['creation_date'])]['orders_sum'] += $order['sum_discount'];
 			}
+			$total_bonus += $order['sum_discount'];
 		}
-		$history[date('d.m.Y', $order['creation_date'])]['orders'][] = $order;
+		$history[date('d.m.Y', $order['creation_date'])]['orders'][$order['creation_date']] = $order;
 	}
 	krsort($history);
+	foreach($history as &$action){
+		krsort($action['orders']);
+	}
 	$tpl->Assign('history', $history);
 }
-
+$tpl->Assign('total_bonus', $total_bonus);
 $agent_users = $Users->GetUsersByAgent($_SESSION['member']['id_user']);
 $tpl->Assign('agent_users', $agent_users);
 $tpl->Assign('msg', array('type' => 'info', 'text' => 'Бонус начисляется только при условии успешного выполнения и рассчитывается с фактической суммы заказа.'));
