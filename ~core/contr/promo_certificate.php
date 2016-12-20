fba047a6c36c4fddaf4323809450717b4c50f2ae
Alexander <arikito.91@gmail.com>
2016-12-20 11:08:16 +0200

    promo_certificate output bug fix

--- ~core/contr/promo_certificate.php
+++ ~core/contr/promo_certificate.php
@@ -3,6 +3,9 @@ $Users->SetFieldsById(isset($_REQUEST['agent'])?$_REQUEST['agent']:$_SESSION['me
 $tpl->Assign('user', $Users->fields);
 $tpl->Assign('customer', $Customers->SetFieldsById($Users->fields['id_user']));
 $gifts = $Products->GetGiftsList('AG'.$Users->fields['id_user']);
+echo '<link rel="stylesheet" href="/themes/default/min/css/fonts.min.css">';
+echo '<link rel="stylesheet" href="/themes/default/min/css/page_styles/promo_certificate.min.css">';
+echo '<div class="main">';
 if(isset($gifts) && !empty($gifts)){
 	foreach($gifts as $gift){
 		$gift['images'] = $Products->GetPhotoById($gift['id_product']);
@@ -12,4 +15,5 @@ if(isset($gifts) && !empty($gifts)){
 }else{
 	echo $tpl->Parse($GLOBALS['PATH_tpl'].'cp_promo_certificate.tpl');
 }
+echo '</div>';
 exit(0);
