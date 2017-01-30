4fd81253f77ba6f15921ee2b56f9eeb863d7b02e
Alexander <arikito.91@gmail.com>
2017-01-30 17:32:26 +0200

    404 page

--- adm/core/block/cp_404.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$parsed_res = array('issuccess' => TRUE,
- 						'html' 		=> $tpl->Parse($GLOBALS['PATH_tpl'].'cp_404.tpl'));
-
-?>
--- ~core/block/cp_404.php
+++ ~core/block/cp_404.php
@@ -1,6 +1,11 @@
-<?php
-
-$parsed_res = array('issuccess' => TRUE,
- 						'html' 		=> $tpl->Parse($GLOBALS['PATH_tpl'].'cp_404.tpl'));
-
+<?php
+
+
+
+$parsed_res = array('issuccess' => TRUE,
+
+ 						'html' 		=> );
+
+
+
 ?>
