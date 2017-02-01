4ee4b9a819343ad91d57231471d5d3d214db9141
Alexander <arikito.91@gmail.com>
2017-02-01 16:09:23 +0200

    Список областей в списке городов

--- adm/core/contr/cities.php
+++ adm/core/contr/cities.php
@@ -6,6 +6,8 @@ $ii = count($GLOBALS['IERA_LINKS']);
 $GLOBALS['IERA_LINKS'][$ii]['title'] = $header;
 $tpl->Assign('h1', $header);
 
+$tpl->Assign('regions', $Address->GetRegionsList());
+
 $tpl->Assign('list', $Address->GetCitiesList());
 
 $tpl_center .= $tpl->Parse($GLOBALS['PATH_tpl'].'cp_cities.tpl');
