34e287bd97263524e3fc9d430968a8a514b56d64
Alexander <arikito.91@gmail.com>
2017-02-01 14:42:28 +0200

    regions list

--- adm/core/contr/regions.php
+++ adm/core/contr/regions.php
@@ -1,32 +1,11 @@
 <?php
+$Address = new Address();
 
-	$ObjName = "Region";	
-	$$ObjName = new Regions();
+$header = 'Области';
+$ii = count($GLOBALS['IERA_LINKS']);
+$GLOBALS['IERA_LINKS'][$ii]['title'] = $header;
+$tpl->Assign('h1', $header);
 
-	// ---- center ----
-	unset($parsed_res);
+$tpl->Assign('list', $Address->GetRegionsList());
 
-	$ii = count($GLOBALS['IERA_LINKS']);
-	$GLOBALS['IERA_LINKS'][$ii]['title'] = "Регионы";
-	$tpl->Assign('h1', $GLOBALS['IERA_LINKS'][$ii]['title']);
-
-	if (isset($_POST['smb']) && isset($_POST['ord'])){
-		$$ObjName->Reorder($_POST);
-		$tpl->Assign('msg', 'Сортировка выполнена успешно.');
-	}
-	
-	if ($$ObjName->SetList()){
-		$tpl->Assign('list', $$ObjName->list);
-	}
-
-	$parsed_res = array('issuccess' => TRUE,
- 						'html' 		=> $tpl->Parse($GLOBALS['PATH_tpl'].'cp_regions.tpl'));
-
-
-	if (TRUE == $parsed_res['issuccess']) {
-		$tpl_center .= $parsed_res['html'];
-	}
-
-	// ---- right ----
-
-?>
+$tpl_center .= $tpl->Parse($GLOBALS['PATH_tpl'].'cp_regions.tpl');
