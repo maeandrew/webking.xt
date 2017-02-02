1e5777343460cfdb6a2eae771e6acd110c0b51be
Alexander <arikito.91@gmail.com>
2017-02-02 17:49:47 +0200

    added comments

--- adm/core/contr/warehouses.php
+++ adm/core/contr/warehouses.php
@@ -6,15 +6,15 @@ $header = 'Пункты выдачи';
 $ii = count($GLOBALS['IERA_LINKS']);
 $GLOBALS['IERA_LINKS'][$ii]['title'] = $header;
 $tpl->Assign('h1', $header);
-
+// Список областей
 $tpl->Assign('regions', $Address->GetRegionsList());
-
+// Список городов
 $tpl->Assign('cities', $Address->GetCitiesList());
-
+// Список ТК
 $tpl->Assign('shipping_companies', $Address->GetShippingCompaniesList());
-
+// Список дилеров
 $tpl->Assign('dealers', $Users->GetDealersList());
-
+// Пагинация
 if(isset($_GET['limit']) && is_numeric($_GET['limit'])){
 	$GLOBALS['Limit_db'] = $_GET['limit'];
 }
@@ -29,8 +29,7 @@ if((isset($_GET['limit']) && $_GET['limit'] != 'all') || !isset($_GET['limit']))
 	$GLOBALS['Limit_db'] = 0;
 	$limit = '';
 }
-
-
+// Список пунктов выдачи
 $tpl->Assign('list', $Address->GetWarehousesList(false, $limit));
 
 $tpl_center .= $tpl->Parse($GLOBALS['PATH_tpl'].'cp_warehouses.tpl');
