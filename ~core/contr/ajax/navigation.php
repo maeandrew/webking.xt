f610c3a63549082c160f6f819c3fc268af698eeb
Webkingdev <webking.dev2@gmail.com>
2017-02-07 18:04:02 +0200

    new nav

--- ~core/contr/ajax/navigation.php
+++ ~core/contr/ajax/navigation.php
@@ -1,6 +1,6 @@
 <?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
     if(isset($_POST['action'])){
         $nav = $Products->generateNavigation($navigation);
-        echo json_encode($nav);
+        echo $nav;
     }
 }
