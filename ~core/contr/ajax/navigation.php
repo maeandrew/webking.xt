0247cf1e65b380e30ba1636f600263ecc5e29c35
Webkingdev <webking.dev2@gmail.com>
2017-02-07 17:19:10 +0200

    navigation.php ajax

---
+++ ~core/contr/ajax/navigation.php
@@ -0,0 +1,27 @@
+<?if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+    if(isset($_COOKIE['Segmentation']) && ($_COOKIE['Segmentation'] == 1 || $_COOKIE['Segmentation'] == 2)) {
+        $segments = $dbtree->Getsegments($_COOKIE['Segmentation']);
+        $segm = '<ul class="second_nav">';
+        foreach($segments as &$v){
+            $segm .= '<li data-id="'.$v['id'].'"'.(isset($GLOBALS['Segment']) && $GLOBALS['Segment'] == $v['id']?' class="active"':null).' onclick="segmentOpen('.$v['id'].')">
+                <span class="link_wrapp">
+                    <a href="#">'.$v['name'].'</a>
+                    <span><i class="material-icons">&#xE315;</i></span>
+                </span>';
+            if(isset($GLOBALS['Segment']) && $GLOBALS['Segment'] == $v['id']){
+                $segm .= $Products->generateNavigation($Products->navigation($v['id']));
+            }
+            $segm .= '</li>';
+        }
+        $segm .= '</ul>';
+
+        $tpl->Assign('nav', $segm);
+    //если куки нету или ее тип = 0, то выводим стандартные категории
+    }else{
+        $tpl->Assign('nav', $Products->generateNavigation($navigation));
+    }
+    $tpl->Assign('sbheader', 'Каталог товаров');
+
+    echo json_encode($tpl);
+
+}
