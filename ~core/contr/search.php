5601045bce659e7db2c2ac494d89addc8546acbd
Alexander <arikito.91@gmail.com>
2017-02-16 13:13:16 +0200

    Search query quotes

--- ~core/contr/search.php
+++ ~core/contr/search.php
@@ -34,7 +34,7 @@ if(isset($_POST['query']) && !isset($_GET['query']) && $_POST['query'] != ''){
 	$query = trim($query);
 }
 G::metaTags(array('page_title' => $header.' по запросу "'.isset($query).'"'));
-$tpl->Assign('header', (isset($query)?'"<b>'.$query.'</b>"':null));
+$tpl->Assign('header', (isset($query)?'&laquo;<b>'.$query.'</b>&raquo;':null));
 if(isset($_SESSION['search']['query']) && isset($query) && $query != '' && $query != $_SESSION['search']['query']){
 	$_SESSION['search']['newsearch'] = 1;
 	$_POST['dropfilters'] = 1;
