e455bc4b0b4f44a3f55b09711f494e2cf6e50dd7
Alexander <arikito.91@gmail.com>
2017-08-11 23:03:41 +0300

    Исправление файла

--- ~core/contr/supplier_search.php
+++
@@ -1,1 +0,0 @@
-<?phpif(!isset($_POST['art_product']) or $_POST['art_product'] == 0){	header('Location: '. _base_url.'/404/');	exit();}$art = $_POST['art_product'];require($GLOBALS['PATH_model'].'invoice_c.php');$invoice = new Invoice();$res = $invoice->GetSuppliersList($art,$arr);$tpl->Assign("result", $res);echo $tpl->Parse($GLOBALS['PATH_tpl'].'supplier_search.tpl');exit(0);?>
